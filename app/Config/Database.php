73f33ca60e5d7127c150c165c2ec71e28b15ee02
kalinux0 <emir-baycan@hotmail.com>
2020-09-12 07:04:52 +0300

    Database changes

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,9 +32,9 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
-		'database' => '',
+		'username' => 'root',
+		'password' => 'sAdece778(&',
+		'database' => 'codeigniter',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
